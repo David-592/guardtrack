7a1eee28a9d5962c30a9cfa3466fbf26b3105987
David-592 <dl6495540@gmail.com>
2026-06-29 11:42:02 -0400

    Update config.h

--- arduino/guardtrack_esp32/config.h
+++ arduino/guardtrack_esp32/config.h
@@ -1,26 +1,32 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define SERVER_URL    "http://your-domain.com/api/data"
-#define KILL_POLL_URL "http://your-domain.com/api/killswitch"
+// --- Server (your live Render dashboard) ---
+#define SERVER_URL       "https://vehicle-immobilizer.onrender.com/api/data"
+#define KILL_POLL_URL    "https://vehicle-immobilizer.onrender.com/api/killswitch"
 
-#define PHONE_1 "+15921234567"
-#define PHONE_2 "+15929876543"
-#define PHONE_3 ""
+// --- Cellular ---
+#define APN              "internet"        // Digicel Guyana
 
-#define SIM_RX     16
-#define SIM_TX     17
-#define SIM_PWRKEY 4
-#define RFID_SS    5
-#define RFID_RST   21
-#define FP_RX      13
-#define FP_TX      12
-#define LED_PIN    2
-#define RELAY_PIN  25
-#define VOLT_PIN   34
+// --- Registered phone numbers (up to 2 now) ---
+#define PHONE_1 "+5926789608"
+#define PHONE_2 ""
 
-#define GPS_INTERVAL    10000
-#define KILL_POLL_MS    5000
-#define LOW_VOLT_THRESH 11.5
+// --- Pin definitions ---
+#define SIM_RX      16     // ESP32 RX  <- SIM7600 TXD
+#define SIM_TX      17     // ESP32 TX  -> SIM7600 RXD
+#define SIM_PWRKEY  4
+#define RFID_SS     5
+#define RFID_RST    21
+#define FP_RX       13
+#define FP_TX       12
+#define LED_PIN     2
+#define RELAY_PIN   25
+#define VOLT_PIN    34
+
+// --- Timing ---
+#define TELEMETRY_INTERVAL_MS  30000UL    // POST heartbeat every 30 s
+#define KILL_POLL_MS           5000UL     // poll kill switch every 5 s
+#define LOW_VOLT_THRESH        11.5
 
 #endif
